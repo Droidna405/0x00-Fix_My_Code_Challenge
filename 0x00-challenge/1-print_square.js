fd0cb627078da34589b575922895101e04072b6c
Droidna405 <joemwangala@gmail.com>
2024-05-24 10:19:59 +0300

    Printing a square using Node.js - debugging

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,18 +14,19 @@ if (process.argv.length !== 3) {
     process.exit(1)
 }
 
-let size = parseInt(process.argv[2])
+const size = parseInt(process.argv[2])
 
-if (isNaN(size)) {
+if (isNaN(size) || size <= 0) {
     process.stderr.write("Invalid size\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n>");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+        row += '#';
     }
-    process.stdout.write("\n");
+    console.log(row);
 }
